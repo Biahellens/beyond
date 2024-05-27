a6df1cd5adee30f059d13810f02782f16034af15
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-05-27 19:28:44 -0300

    :sparkles: feat(): Adicionando mobileContext

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MobileProvider } from "@contexts/MobileContext.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MobileProvider>
+        <App />
+    </MobileProvider>
+  </React.StrictMode>
+);
