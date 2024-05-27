b97f035ada0187a34dd094f0b57ad29571e05b62
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-05-27 19:29:50 -0300

    :sparkles: feat(): Adicionando header

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,69 @@
+import { UseMobile } from "@contexts/MobileContext";
+import { Box, Drawer, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import './style.css'
+
+// Icons
+import icon_climagro from "@assets/icon_climagro.svg";
+import PlaceIcon from '@mui/icons-material/Place';
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const { isMobile } = UseMobile();
+  const navigate = useNavigate()
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  return (
+    <>
+      <Stack
+        onClick={toggleDrawer(true)}
+        width="100vw"
+        height="5rem"
+        sx={{
+          background: "#E0FFFF",
+          padding: "2rem",
+          top: 0,
+          position: isMobile ? '' : "fixed",
+          justifyContent: isMobile ? "center" : "end",
+          display: "flex",
+          alignItems: isMobile ? "center" : "end",
+          flexDirection: 'row'
+        }}
+      >
+        <Typography ml={1}>DellBeyond</Typography>
+        <PlaceIcon />
+      </Stack>
+      {isMobile &&
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <Box role="presentation" onClick={toggleDrawer(false)}>
+            <Stack
+              sx={{
+                width: "100%",
+              }}
+            >
+              <Stack
+                onClick={() => navigate('/mapDelivery')}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: "0.5rem",
+                  cursor: "pointer",
+                }}
+              >
+                <PlaceIcon />
+                <Typography ml={1}>Endereços</Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Drawer>
+      }
+    </>
+  );
+}
